351e5eeb70aee0bfb20783e0117998692f8249a6
athulyaliving1 <110807506+athulyaliving1@users.noreply.github.com>
2022-09-23 16:51:51 +0530

    contact input fix

--- src/components/Basic/MdForm.js
+++ src/components/Basic/MdForm.js
@@ -95,7 +95,7 @@ function MdForm() {
                   id="name"
                   onChange={(e) => setName(e.target.value)}
                   placeholder="Enter Your Name"
-                  className="w-100 mt-2 py-3 rounded-lg bg-white border border-gray-400 font-semibold focus:border-sky-700 focus:outline-none placeholder:px-5"
+                  className="form-control block w-full px-3 py-3 mt-3   font-semibold text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-pink-500 focus:outline-none"
                   name="name"
                 />
                 <p className="text-pink-500 font-Ubuntu font-semibold">
@@ -110,7 +110,7 @@ function MdForm() {
                   id="email"
                   onChange={(e) => setEmail(e.target.value)}
                   placeholder="Enter Your Email"
-                  className="w-100 mt-2 py-3 rounded-lg bg-white border border-gray-400 font-semibold focus:border-sky-700 focus:outline-none placeholder:px-5 "
+                  className="form-control block w-full px-3 py-3 mt-3   font-semibold text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-pink-500 focus:outline-none "
                   name="email"
                 />
                 <p className="text-pink-500  font-semibold">
@@ -125,7 +125,7 @@ function MdForm() {
                   id="number"
                   onChange={(e) => setNumber(e.target.value)}
                   placeholder="Enter Your Mobile number"
-                  className="w-100 mt-2 py-3 rounded-lg bg-white border border-gray-400 font-semibold focus:border-sky-700 focus:outline-none placeholder:px-5"
+                  className="form-control block w-full px-3 py-3 mt-3   font-semibold text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-pink-500 focus:outline-none"
                   name="number"
                 />
                 <p className="text-pink-500  font-semibold">
@@ -140,7 +140,7 @@ function MdForm() {
                   onChange={(e) => setTextarea(e.target.value)}
                   placeholder="Enter Your Message"
                   rows="1"
-                  class="w-100 mt-2 py-3 px-3 rounded-lg bg-white  border border-gray-400   font-semibold focus:border-sky-700 focus:outline-none "
+                  class="form-control block w-full px-3 py-3 mt-3    font-semibold text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-pink-500 focus:outline-none"
                   name="textarea"
                 />
                 <p className="text-pink-500  font-semibold">
